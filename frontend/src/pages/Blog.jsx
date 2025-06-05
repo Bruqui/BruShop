ec2b32252c0179ec6b49e38180b0b143812a7548
Bruqui <dylan2003gd@gmail.com>
2025-06-05 23:38:12 +0200

    add space in the file

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
-import {blogs} from '../assets/data'
+import { blogs } from '../assets/data'
 
 const Blog = () => {
   return (
     <section className='max-padd-container pb-16'>
       {/** CONTAINER */}
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5'>
-        {blogs.slice(0, 4).map((blog)=>(
+        {blogs.slice(0, 4).map((blog) => (
           <div key={blog.image} className='relative'>
-            <img src={blog.image} alt='blocImg' className='rounded-xl'/>
+            <img src={blog.image} alt='blocImg' className='rounded-xl' />
             {/** INFO */}
-              <p className='medium-14 mt-6'>
-                {blog.category}
-              </p>
-              <h5 className='h5 pr-4 mb-1'>
-                {blog.title}
-              </h5>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, voluptate modi!
-              </p>
-              <button className='underline mt-2 bold-14'>
-                continue reading
-              </button>
+            <p className='medium-14 mt-6'>
+              {blog.category}
+            </p>
+            <h5 className='h5 pr-4 mb-1'>
+              {blog.title}
+            </h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, voluptate modi!
+            </p>
+            <button className='underline mt-2 bold-14'>
+              continue reading
+            </button>
           </div>
         ))}
       </div>
