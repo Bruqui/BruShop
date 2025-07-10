73fa0fdcdd12d3ebb803fc28e64e12e5c3836aa7
Bruqui <dylan2003gd@gmail.com>
2025-07-11 01:07:41 +0200

    Added error handling in the addToCart function and added the getCartCount function to calculate the total number of items in the cart

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react'
 import { products } from '../assets/data'
 import { useNavigate } from "react-router-dom"
+import { toast } from "react-toastify"
 
 export const ShopContext = createContext();
 
@@ -11,7 +12,13 @@ const ShopContextProvider = (props) => {
     const delivery_charges = 10
     const [cartItems, setCartItems] = useState({});
 
+    // ADDING ITEMS TO CART
     const addToCart = async (itemId, color) => {
+        if (!color) {
+            toast.error("Please Select the color first");
+            return;
+        }
+
         let cartData = structuredClone(cartItems);
         if (cartData[itemId]) {
             if (cardData[itemId][color]) {
@@ -26,11 +33,26 @@ const ShopContextProvider = (props) => {
         setCartItems(cartData);
     };
 
+    // GETTING TOTAL CART COUNT
+    const getCartCount = async () => {
+        let totalCount = 0
+        for (const items in cartItems) {
+            for (const item in cartItems[items]) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalCount += cartItems[items][item];
+                    }
+                } catch (error) { }
+            }
+        }
+        return totalCount;
+    };
+
     useEffect(() => {
         console.log(cartItems);
     }, [cartItems]);
 
-    const value = { products, search, setSearch, currency, delivery_charges, cartItems, setCartItems, addToCart };
+    const value = { products, search, setSearch, currency, delivery_charges, cartItems, setCartItems, addToCart, getCartCount };
     return (
         <ShopContext.Provider value={value}>
             {props.children}
