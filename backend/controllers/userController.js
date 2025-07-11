517ff0b2e16ef554f85b5f3d8e24a7d03c7b878e
Bruqui <dylan2003gd@gmail.com>
2025-07-11 20:21:25 +0200

    implement user login with password check and JWT token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,25 @@ const createToken = (id) => {
 
 // CONTROLLER FUNCTION FOR USER LOGIN
 const loginUser = async (req, res) => {
-    res.json({ message: "this is login controller function for login api" })
+    try {
+        const { email, password } = req.body
+        const user = await userModel.findOne({ email })
+        if (!user) {
+            return res.json({ success: false, message: "User doesn't exists" })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (isMatch) {
+            const token = createToken(user._id)
+            res.json({ success: true, token })
+        }
+        else {
+            res.json({ success: false, message: "Invalid Credentials" })
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 }
 
 // CONTROLLER FUNCTION FOR USER REGISTER
