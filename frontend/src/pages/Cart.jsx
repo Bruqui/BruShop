e991c10108fc77263feed0fe819452f9c7b4de22
Bruqui <dylan2003gd@gmail.com>
2025-07-11 03:36:10 +0200

    Added item management in the Cart component with buttons to increase/decrease quantity and integrated icons. Improved render structure and added the Footer component

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,6 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Titles from '../components/Titles';
+import { FaRegWindowClose } from 'react-icons/fa';
+import { FaMinus, FaPlus } from 'react-icons/fa6';
+import CartTotal from '../components/CartTotal';
+import Footer from '../components/Footer';
 
 const Cart = () => {
     const { products, currency, cartItems, getCartCount } = useContext(ShopContext);
@@ -31,16 +35,61 @@ const Cart = () => {
     }, [cartItems, products]);
 
     return (
-        <div>
-            <div>
-                <div>
-                    <div>
+        <section>
+            <div className='bg-primary'>
+                <div className='max-padd-container py-10'>
+                    {/** TITLES */}
+                    <div className='flexStart gap-x-4'>
                         <Titles title1={"Cart"} title2={"List"} title1Styles={'h3'} />
-                        <h5>({getCartCount()})</h5>
+                        <h5 className='medium-15 text-gray-30 relative'>
+                            ({getCartCount()} Items)
+                        </h5>
+                    </div>
+                    {/** CONTAINER */}
+                    <div>
+                        {cartData.map((item, i) => {
+                            const productData = products.find((product) => product._id === item._id)
+                            const key = `${item._id}-${item.color}`
+                            return (
+                                <div key={i}>
+                                    <div>
+                                        <div>
+                                            <img src={productData.image[0]} alt="productImg" className='w-20 sm:w-18 rounded' />
+                                        </div>
+                                        <div>
+                                            <div>
+                                                <h5>{productData.name}</h5>
+                                                <FaRegWindowClose />
+                                            </div>
+                                            <p>{item.color}</p>
+                                            <div>
+                                                <div>
+                                                    <button>
+                                                        <FaMinus />
+                                                    </button>
+                                                    <p>{quantities[key]}</p>
+                                                    <button>
+                                                        <FaPlus />
+                                                    </button>
+                                                </div>
+                                                <h4>{currency}{productData.price}</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div>
+                        <div>
+                            <CartTotal />
+                            <button>Proceed to Checkout</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <Footer />
+        </section>
     )
 };
 
