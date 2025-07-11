14be9e69fbc984b9f7bac60d0abc8a6e2dd7ccdd
Bruqui <dylan2003gd@gmail.com>
2025-07-11 22:05:23 +0200

    implement addProduct controller with image upload to Cloudinary

---
+++ backend/controllers/productController.js
@@ -0,0 +1,64 @@
+import { v2 as cloudinary } from "cloudinary"
+import productModel from "../models/productModel.js"
+
+// CONTROLLER FUNCTION FOR ADDING PRODUCT
+const addProduct = async (req, res) => {
+    try {
+        const { name, description, price, category, colors, popular } = req.body
+
+        // EXTRACTING IMAGES IF PROVIDED
+        const image1 = req.files?.image1?.[0];
+        const image2 = req.files?.image2?.[0];
+        const image3 = req.files?.image3?.[0];
+        const image4 = req.files?.image4?.[0];
+
+        const images = [image1, image2, image3, image4].filter((item) => item !== undefined)
+
+        //UPLOAD IMAGES TO CLOUDINARY OR USE A DEFAULT IMAGE
+        let imagesUrl;
+        if (images.length > 0) {
+            imagesUrl = await Promise.all(
+                images.map(async (item) => {
+                    const result = await cloudinary.uploader.upload(item.path, { resource_type: "image" })
+                    return result.secure_url
+                })
+            )
+        } else {
+            // DEFAULT IMAGE URL IF NO IMAGES ARE PROVIDED
+            imagesUrl = ['https://via.placeholder.com/15O']
+        }
+
+        // CREATE PRODUCT DATA
+        const productData = {
+            name,
+            description,
+            price,
+            category,
+            popular: popular == "true" ? true : false,
+            colors: colors ? JSON.parse(colors) : [], // DEFAULT TO EMPTY ARRAY IF COLORS NOT PROVIDED
+            image: imagesUrl,
+            date: Date.now()
+        }
+
+        console.log(productData)
+
+        const product = new productModel(productData)
+        await product.save()
+
+        res.json({ success: true, message: "Product Added" })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// CONTROLLER FUNCTION FOR ADDING PRODUCT
+const removeProduct = async (req, res) => { }
+
+// CONTROLLER FUNCTION FOR ADDING PRODUCT
+const listProduct = async (req, res) => { }
+
+// CONTROLLER FUNCTION FOR ADDING PRODUCT
+const singleProduct = async (req, res) => { }
+
+export { addProduct, removeProduct, listProduct, singleProduct }
