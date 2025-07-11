4ecd6ba514774dbb7991ebeb8e28ae69d170400c
Bruqui <dylan2003gd@gmail.com>
2025-07-11 21:26:08 +0200

    fix product routes import and export

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,11 @@
 import express from "express"
-import { addProduct, listProduct, removeProduct, singleProduct } from "../controllers/productController"
+import { addProduct, listProduct, removeProduct, singleProduct } from "../controllers/productController.js"
 
 const productRouter = express.Router()
 
-productRouter.post("/add",addProduct)
-productRouter.post("/remove",removeProduct)
-productRouter.post("/single",singleProduct)
-productRouter.get("/list",listProduct)
+productRouter.post("/add", addProduct)
+productRouter.post("/remove", removeProduct)
+productRouter.post("/single", singleProduct)
+productRouter.get("/list", listProduct)
 
 export default productRouter
