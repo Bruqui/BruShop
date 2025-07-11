12a6a94b736922df4747d19832699e300824fb0d
Bruqui <dylan2003gd@gmail.com>
2025-07-11 02:44:54 +0200

    Added cart management logic in the Cart component, including the use of ShopContext and handling of cart items

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+    const { products, currency, cartItems } = useContext(ShopContext);
 
-export default Cart
+    const [cartData, setCartData] = useState([])
+    const [quantities, setQuantities] = useState({})
+    useEffect(() => {
+        if (products.length > 0) {
+            const tempData = []
+            const initialQuantities = {}
+            for (const items in cartItems) {
+                for (const item in cartItems[items]) {
+                    if (cartItems[items][item] > 0) {
+                        tempData.push({
+                            _id: items,
+                            color: item,
+                            quantity: cartItems[items][item]
+                        })
+                        initialQuantities[`${item}-${item}`] = cartItems[items][item]
+                    }
+                }
+            }
+            console.log(tempData);
+        }
+    }, [cartItems])
+
+    return <div>Cart</div>
+};
+
+
+export default Cart;
