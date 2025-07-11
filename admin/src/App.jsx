1a92ffed47e85998ebd80e4b934c05027da4da82
Bruqui <dylan2003gd@gmail.com>
2025-07-12 00:19:05 +0200

    replace Vite default UI with main app layout and routing

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { ToastContainer } from "react-toastify"
+import Login from "./pages/Login";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [token, setToken] = useState("sdfg")
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <ToastContainer />
+      {token === "" ? (
+        <Login setToken={setToken} />
+      ) : (
+        <div className="bg-primary text-[#404040]">
+          <div className="mx-auto max-w-[1440px] flex flex-col sm:flex-row">
+            <Sidebar />
+            <Routes>
+              <Route path="/" element={<Add />} />
+              <Route path="/list" element={<List />} />
+              <Route path="/orders" element={<Orders />} />
+            </Routes>
+          </div>
+        </div>
+      )}
+    </main>
   )
 }
-
-export default App
