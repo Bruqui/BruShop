bcbfcda2210ca30117ca9253935a20916c05663d
Bruqui <dylan2003gd@gmail.com>
2025-07-11 22:24:09 +0200

    implement removeProduct controller to delete a product by ID

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -53,7 +53,15 @@ const addProduct = async (req, res) => {
 }
 
 // CONTROLLER FUNCTION FOR ADDING PRODUCT
-const removeProduct = async (req, res) => { }
+const removeProduct = async (req, res) => {
+    try {
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({ success: true, message: "Product Remove" })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
 
 // CONTROLLER FUNCTION FOR ADDING PRODUCT
 const listProduct = async (req, res) => {
