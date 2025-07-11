1b4507e92f82b38c4f7fd43ac834b1f7859b6468
Bruqui <dylan2003gd@gmail.com>
2025-07-11 14:32:59 +0200

    Added cart total management: integrated subtotal, shipping fee, and total amounts using the ShopContext

--- frontend/src/components/CartTotal.jsx
+++ frontend/src/components/CartTotal.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Titles from "./Titles";
 
 const CartTotal = () => {
+  const { currency, getCartAmount, delivery_charges } = useContext(ShopContext)
+
   return (
-    <div>CartTotal</div>
+    <section className="w-full">
+      <Titles title1={'Cart'} title2={'Total'} title1Styles={'h3'} />
+      <div className="flexBetween pt-3">
+        <h5 className="h5">SubTotal:</h5>
+        <p className="h5">{currency}{getCartAmount()}.00</p>
+      </div>
+      <hr className="mx-auto h-[1px] w-full bg-gray-900/5 my-1" />
+      <div className="flexBetween pt-3">
+        <h5 className="h5">Shipping Fee:</h5>
+        <p className="h5">{getCartAmount() === 0 ? "0.00" : `${currency}${delivery_charges}.00`}</p>
+      </div>
+      <hr className="mx-auto h-[1px] w-full bg-gray-900/5 my-1" />
+      <div className="flexBetween pt-3">
+        <h5 className="h5">Total:</h5>
+        <p className="h5">{currency}{getCartAmount() === 0 ? "0.00" : getCartAmount() + delivery_charges}</p>
+      </div>
+    </section>
   )
 }
 
-export default CartTotal
+export default CartTotal;
