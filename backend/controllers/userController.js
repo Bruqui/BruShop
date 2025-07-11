50c0fc53a8743b561088ddbf5999d9b362e781fb
Bruqui <dylan2003gd@gmail.com>
2025-07-11 19:14:36 +0200

    add auth controller functions (login, register, admin login)

---
+++ backend/controllers/userController.js
@@ -0,0 +1,8 @@
+// CONTROLLER FUNCTION FOR USER LOGIN
+const loginUser = async (req, res) => { }
+
+// CONTROLLER FUNCTION FOR USER REGISTER
+const REGISTERUser = async (req, res) => { }
+
+// CONTROLLER FUNCTION FOR ADMIN LOGIN
+const adminLogin = async (req, res) => { }
