7626de74e53588f1ee44e089bc99a5baf4c2190f
Bruqui <dylan2003gd@gmail.com>
2025-07-11 23:34:06 +0200

    add adminAuth middleware to protect add and remove product routes

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { addProduct, listProduct, removeProduct, singleProduct } from "../controllers/productController.js"
 import uqload from "../middleware/multer.js"
+import adminAuth from "../middleware/adminAuth.js"
 
 const productRouter = express.Router()
 
-productRouter.post("/add", uqload.fields([{ name: 'image1', maxCount: 1 }, { name: 'image2', maxCount: 1 }, { name: 'image3', maxCount: 1 }, { name: 'image4', maxCount: 1 }]), addProduct)
-productRouter.post("/remove", removeProduct)
+productRouter.post("/add", adminAuth, uqload.fields([{ name: 'image1', maxCount: 1 }, { name: 'image2', maxCount: 1 }, { name: 'image3', maxCount: 1 }, { name: 'image4', maxCount: 1 }]), addProduct)
+productRouter.post("/remove", adminAuth, removeProduct)
 productRouter.post("/single", singleProduct)
 productRouter.get("/list", listProduct)
 
