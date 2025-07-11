4cf767b1039ed8f29359da77f1d2daa8aeba61ee
Bruqui <dylan2003gd@gmail.com>
2025-07-11 19:23:15 +0200

    add user and admin auth routes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,10 @@
 import express from "express"
+import { adminLogin, loginUser, registerUser } from "../controllers/userController"
 
 const userRouter = express.Router()
+
+userRouter.post('/register', registerUser)
+userRouter.post('/login', loginUser)
+userRouter.post('/admin', adminLogin)
 
 userRouter.post('/register', register)
