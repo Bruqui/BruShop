87189197629394ad56b9f72ca838b2c952e22c4f
Bruqui <dylan2003gd@gmail.com>
2025-07-11 04:13:59 +0200

    Fixed indentation and updated the title style in the PopularProducts component

--- frontend/src/components/PopularProducts.jsx
+++ frontend/src/components/PopularProducts.jsx
@@ -5,7 +5,7 @@ import { ShopContext } from '../context/ShopContext';
 
 const PopularProducts = () => {
   const [popularProducts, setPopularProducts] = useState([])
-  const {products} = useContext(ShopContext)
+  const { products } = useContext(ShopContext)
 
   useEffect(() => {
     const data = products.filter(item => item.popular)
@@ -14,7 +14,7 @@ const PopularProducts = () => {
 
   return (
     <section className='max-padd-container pt-16'>
-      <Titles title1={"Popular"} title2={"Products"} titleStyles={"pb-10"} paraStyles={"!block"} />
+      <Titles title1={"Popular"} title2={"Products"} titleStyles={"pb-14"} paraStyles={"!block"} />
       {/** CONTAINER */}
       <div className='grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8'>
         {popularProducts.map(product => (
