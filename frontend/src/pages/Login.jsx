cb1f7263f65b3942ed67107271b74de824f880ec
Bruqui <dylan2003gd@gmail.com>
2025-07-13 01:46:25 +0200

    implement login and signup functionality with API integration

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,50 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import login from "../assets/login.png"
+import { ShopContext } from '../context/ShopContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Login = () => {
   const [currState, setCurrState] = useState('Sign Up')
+  const { token, setToken, navigate, backendUrl } = useContext(ShopContext)
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault()
+    try {
+      if (currState === "Sign Up") {
+        // SIGN UP API
+        const response = await axios.post(backendUrl + '/api/user/register', { name, email, password })
+        if (response.data.success) {
+          setToken(response.data.token)
+          localStorage.setItem("token", response.data.token);
+        } else {
+          toast.error(response.data.message)
+        }
+      } else {
+        // LOGIN API
+        const response = await axios.post(backendUrl + "/api/user/login", { email, password })
+        if (response.data.success) {
+          setToken(response.data.token)
+          localStorage.setItem("token", response.data.token)
+        } else {
+          toast.error(response.data.message)
+        }
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(() => {
+    if (token) {
+      navigate('/')
+    }
+  }, [token])
 
   return (
     <div className='absolute top-0 left-0 h-full w-full z-50 bg-white'>
@@ -14,7 +56,7 @@ const Login = () => {
         </div>
         {/** FORM SIDE */}
         <div className='flexCenter w-full sm:w-1/2 text-[90%]'>
-          <form className='flex flex-col items-center w-[90%] sm:max-w-md m-auto gap-y-5'>
+          <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-md m-auto gap-y-5'>
             <div className='w-full mb-4'>
               <h3 className='bold-36'>
                 {currState}
@@ -25,16 +67,20 @@ const Login = () => {
                 <label htmlFor="name" className='medium-15'>
                   Name
                 </label>
-                <input type="text" placeholder='Name' className='w-full px-3 py-1.5 ring-1 ring-slate-900/5 rounded bg-primary mt-1' />
+                <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder='Name' className='w-full px-3 py-1.5 ring-1 ring-slate-900/5 rounded bg-primary mt-1' />
               </div>
             )}
             <div className='w-full'>
-              <label htmlFor="email" className='medium-15'>Email</label>
-              <input type="text" placeholder='Email' className='w-full px-3 py-1.5 ring-1 ring-slate-900/5 rounded bg-primary mt-1' />
+              <label htmlFor="email" className='medium-15'>
+                Email
+              </label>
+              <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" placeholder='Email' className='w-full px-3 py-1.5 ring-1 ring-slate-900/5 rounded bg-primary mt-1' />
             </div>
             <div className='w-full'>
-              <label htmlFor="password" className='medium-15'>Password</label>
-              <input type="text" placeholder='Password' className='w-full px-3 py-1.5 ring-1 ring-slate-900/5 rounded bg-primary mt-1' />
+              <label htmlFor="password" className='medium-15'>
+                Password
+              </label>
+              <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" placeholder='Password' className='w-full px-3 py-1.5 ring-1 ring-slate-900/5 rounded bg-primary mt-1' />
             </div>
             <button type='submit' className='btn-dark w-full mt-5 !py-[8px] !rounded'>
               {currState === "Sign Up" ? "Sign Up" : "Login"}
